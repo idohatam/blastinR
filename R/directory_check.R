4aa597980ec749f8e0561fe1ad7d40e1b049f0a6
Ido Hatam <ihatam@BIOL-67714-L.local>
2025-05-07 13:43:42 -0700

    Added a check that creates an output directory for blast db.

--- R/directory_check.R
+++ R/directory_check.R
@@ -1,8 +1,12 @@
 #' Check and Create Output Directories
 #'
 #' This internal helper function ensures that the necessary output directories
-#' (`outputs/html`, `outputs/table`, `outputs/hits`) exist within the working
+#' (`outputs/html`, `outputs/table`, `outputs/hits`, `outputs/db`) exist within the working
 #' directory. If any of them are missing, it creates them.
+#' html stores the html report along with any figure that's generated
+#' table stores any table that is generated
+#' hits stores any exported fasta files with hits from blast searches
+#' db stores databse files outputed from blastdb
 #'
 #' @keywords internal
 #' @noRd
@@ -10,15 +14,15 @@
 directory_check <- function(){
   # Define the base directory
   base_dir <- "outputs"
-  
+
   # List of sub-directories to create
-  subdirs <- c("html", "table", "hits")
-  
+  subdirs <- c("html", "table", "hits","db")
+
   # Create base directory if it does not exist
   if (!file.exists(base_dir)) {
     dir.create(base_dir)
   }
-  
+
   # Loop through and create each sub-directory if it does not exist
   for (subdir in subdirs) {
     subdir_path <- file.path(base_dir, subdir)
